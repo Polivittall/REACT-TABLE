86d1911458468d6f15121c10a603a6a1c6856aff
Poliana Vital <poliana@vita.oim>
2024-03-27 21:59:23 -0300

    Aula 4

---
+++ src/components/PrinceListComponents.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+function PrinceList (){
+
+    let valor_a = 4
+    let valor_b = 2
+    let [Total, SetTotal] = useState(valor_a + valor_b)
+
+function acaobotao(){
+    SetTotal(Total + 1)
+    console.log("Total");
+}
+
+return(
+
+    <>
+        <table>
+            <tbody>
+            <tr>
+                <td>Valor 1</td>
+                <td>Valor 2 </td>
+                <td>Total</td>
+            </tr>
+            <tr>
+                <td>{valor_a}</td>
+                <td>{valor_b}</td>
+                <td>{Total}</td>
+            </tr>
+            </tbody>
+        </table>
+        <br />
+        <Button onClick={acaobotao} variant="primary">Primary</Button>
+       <button > Somar 1 no Total</button>
+    
+    </>
+)
+
+}
+
+export default PrinceList;
---
+++ src/pages/PrincipalPage.js
@@ -0,0 +1,14 @@
+import PrinceList from "../components/PrinceListComponents";
+
+function PrincipalPage(){
+    return(
+        <>
+            Hello World - Table 
+            
+            <PrinceList></PrinceList>
+        </>
+    );
+
+}
+
+export default PrincipalPage;
